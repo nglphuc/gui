4393dd99a9a8f4724b8bec5fd07a3a539c15d5ce
nglphuc <nguyenluongphuc2009@gmail.com>
2024-12-03 19:24:00 +0700

    Update bai1.py

--- bai1.py
+++ bai1.py
@@ -3,7 +3,7 @@ from guizero import *
 app = App(title= "hello world")
 title = Text(app, text= "make AI helpful for everyone")
 text1 = Text(app, text= "donec magna orci, molestie a enim a, dapibus tincidunt nulla. Mauris nunc ligula, tincidunt")
-picture = Picture(app, image="gui/rizzler.png", width=500, height=600)
+picture = Picture(app, image="rizzler.png", width=500, height=600)
 text2 = Text(app, text="donec magna orci, molestie a enim a, dapibus tinci")
 text3 = Text(app, text="VTV.vn - Hơn nửa năm qua, hàng loạt đại án tham nhũng trong nhiều cấp, nhiều ngành, nhiều lĩnh vực đã bị phanh phui, điều tra, xử lý kiên quyết. 27 cán bộ thuộc diện Bộ Chính trị, Ban Bí thư quản lý bị kỷ luật từ đầu năm")
  
